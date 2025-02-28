d2f09b6907d80c698bda2032999765bb67052c8f
prakul19 <prakulagarwal5@gmail.com>
2025-02-28 10:30:11 +0530

    [Prakul] stats: program for finding sam's average marks in pcm

---
+++ SamMarksAverage.java
@@ -0,0 +1,9 @@
+public class SamMarksAverage {
+	public static void main (String[] args) {
+		int mathMarks = 94;
+		int physicsMarks = 95;
+		int chemistryMarks = 96;
+		double marksAverage = (double) (mathMarks + physicsMarks + chemistryMarks) / 3;
+		System.out.print("Sam's average mark in PCM is " + marksAverage);  
+	}
+}
